2c457f9f30aea056db81d1b47a356480ecf57650
Liran Attar <lirattar@gmail.com>
2026-03-23 02:03:52 +0200

    Add daily trading job to jobs.py

--- jobs.py
+++ jobs.py
@@ -14,3 +14,6 @@ async def bootstrap_history_job(data_feed: DataFeed, db: DB) -> None:
 
 async def daily_incremental_update_job(data_feed: DataFeed) -> None:
     await data_feed.daily_incremental_update()
+
+async def daily_trading_job(portfolio: Portfolio) -> None:
+    await portfolio.run_cycle()
