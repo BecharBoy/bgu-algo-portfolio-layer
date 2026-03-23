edf71c628c9b30b5f79bacfc3da894c25c772827
Liran Attar <lirattar@gmail.com>
2026-03-23 02:02:39 +0200

    Import DB and logging in jobs.py
    Added DB import and logging for bootstrap job.

--- jobs.py
+++ jobs.py
@@ -1,6 +1,7 @@
 from DataFeed import DataFeed
 from Portfolio import Portfolio
-
+from DB import DB
+import logging
 
 async def bootstrap_history_job(data_feed: DataFeed, db: DB) -> None:
     already_done = await db.get_system_flag("bootstrap_done")
@@ -13,7 +14,3 @@ async def bootstrap_history_job(data_feed: DataFeed, db: DB) -> None:
 
 async def daily_incremental_update_job(data_feed: DataFeed) -> None:
     await data_feed.daily_incremental_update()
-
-
-async def daily_trading_job(portfolio: Portfolio]) -> None:
-    await portfolio.run_cycle()
